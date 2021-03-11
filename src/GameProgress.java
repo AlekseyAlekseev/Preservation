bd435f00995dc5a57aa4b8841e202c821d23b8de
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-03-11 15:55:23 +0300

    last commit

---
+++ src/GameProgress.java
@@ -0,0 +1,64 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class GameProgress implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int health;
+    private int weapons;
+    private int lvl;
+    private double distance;
+
+    public GameProgress(int health, int weapons, int lvl, double distance) {
+        this.health = health;
+        this.weapons = weapons;
+        this.lvl = lvl;
+        this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return "GameProgress{" +
+                "health=" + health +
+                ", weapons=" + weapons +
+                ", lvl=" + lvl +
+                ", distance=" + distance +
+                '}';
+    }
+
+    public static void saveGame(String savePath, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(savePath);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void zipFiles(String zipPath, String filePath) {
+        try (ZipOutputStream zout = new ZipOutputStream(new
+                FileOutputStream(zipPath));
+             FileInputStream fis = new FileInputStream(filePath)) {
+            ZipEntry entry = new ZipEntry("packed_save.dat");
+            zout.putNextEntry(entry);
+            // считываем содержимое файла в массив byte
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            // добавляем содержимое к архиву
+            zout.write(buffer);
+            // закрываем текущую запись для новой записи
+            zout.closeEntry();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        GameProgress gameProgressJhon = new GameProgress(91, 7, 15, 150.27);
+        GameProgress gameProgressMike = new GameProgress(55, 3, 23, 257.56);
+        GameProgress gameProgressTony = new GameProgress(100, 9, 7, 97.23);
+
+        GameProgress.saveGame("C://JAVA//Installation//Games//savegames//save1.dat", gameProgressJhon);
+        GameProgress.saveGame("C://JAVA//Installation//Games//savegames//save2.dat", gameProgressMike);
+        GameProgress.saveGame("C://JAVA//Installation//Games//savegames//save3.dat", gameProgressTony);
+
+        GameProgress.zipFiles("C://JAVA//Installation//Games//savegames//save1.zip",
+                "C://JAVA//Installation//Games//savegames//save1.dat");
+
+        GameProgress.zipFiles("C://JAVA//Installation//Games//savegames//save2.zip",
+                "C://JAVA//Installation//Games//savegames//save2.dat");
+
+        GameProgress.zipFiles("C://JAVA//Installation//Games//savegames//save3.zip",
+                "C://JAVA//Installation//Games//savegames//save3.dat");
+
+
     }
+
+
+
+
 }
